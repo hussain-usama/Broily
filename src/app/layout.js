707fc15a1fb5850326c2f1c26cdd22f333ab6ef9
Usama Hussain <usamaahussain23@gmail.com>
2025-04-13 02:42:00 +0500

    checking full screen fit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import Script from "next/script"; 
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,25 +27,26 @@ export default function RootLayout({ children }) {
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-FZJBDNFE28"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-FZJBDNFE28');
-          `}
-        </Script>
         <Script id="viewport-meta" strategy="beforeInteractive">
-  {`
+          {`
     const meta = document.createElement('meta');
     meta.name = 'viewport';
     meta.content = 'width=device-width, initial-scale=1, viewport-fit=cover';
     document.head.appendChild(meta);
   `}
-</Script>
+        </Script>
+
+        <Script id="theme-color" strategy="beforeInteractive">
+          {`
+    const meta = document.createElement('meta');
+    meta.name = 'theme-color';
+    meta.content = '#F7FF58';
+    document.head.appendChild(meta);
+  `}
+        </Script>
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F7FF58] pt-[env(safe-area-inset-top)] min-h-screen`}
       >
         {children}
       </body>
